4eb72b34865c59cf7407432711f84bec196333da
Jaime Colman <mjackerjaime@gmail.com>
2023-02-23 23:34:38 +0800

    testing gcc compiler.

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/****************************************************************/
+/* Name: Hello.c*/
+/* Purpose: testing compiler gcc	*/
+/* Author: Jaime								*/
+/****************************************************************/
+
+int main(void) 
+{
+	printf("Hello NTUST");
+	
+	return 0;
+}
